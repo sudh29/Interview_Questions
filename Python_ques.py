3490f9a67cbfdf51e23abbbce30f946ae3fab67f
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2022-10-12 18:46:37 +0530

    Update Python_ques.py

--- Python_ques.py
+++ Python_ques.py
@@ -3,3 +3,12 @@ from functools import reduce
 new=[1,2,3,4,5]
 res = reduce(lambda a,b : a+b,new)
 print(res)
+
+# swap two number without using two numbers
+a=7
+b=12
+print(a,b)
+a=a+b #19
+b=a-b #7
+a=a-b #12
+print(a,b)
