f18e853a5af43c1ddf6ee40840b0ed59723197e0
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2022-10-12 19:47:40 +0530

    Update Python_ques.py

--- Python_ques.py
+++ Python_ques.py
@@ -12,3 +12,14 @@ a=a+b #19
 b=a-b #7
 a=a-b #12
 print(a,b)
+
+# Capitalize first letter
+str_a = "sudhanshu"
+print(str_a)
+str_b=str_a.capitalize()
+print(str_b)
+
+# Difference in sort and sorted
+'''The sorted() function returns a sorted list of the specific iterable object.
+    The sort() method sorts the list.'''
+
