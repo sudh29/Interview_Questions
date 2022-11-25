c42acc8890eac84aa1c4dfd7f6f201d95b07c3ce
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2022-11-25 12:28:16 +0530

    Update Python_ques.py

--- Python_ques.py
+++ Python_ques.py
@@ -90,8 +90,59 @@ except MyError as error:
 # CPU details linux command
 # lscpu
 
+'''JSON is a pure string written in a convention format, which does not have any characteristics of data structure
+. ... Python's dictionary key can be any hash object, and JSON can only be a string. 
+The Python dict string uses single quotation marks, and JSON enforces double quotation marks. 
+You can nest tuple in Python dict.
+JSON dictionary must have a time complexity of O(n)
+Python dictionary, and now finding a key has a time complexity of O(1)'''
 
 
+
+
+'''A class method receives the class as an implicit first argument, just like an instance method receives the instance 
+A class method is a method that is bound to the class and not the object of the class.
+It can modify a class state that would apply across all the instances of the class. 
+For example, it can modify a class variable that will be applicable to all the instances
+
+A static method does not receive an implicit first argument.
+A static method is also a method that is bound to the class and not the object of the class.
+This method can’t access or modify the class state
+
+Instance Method:
++ Can modify object instance state
++ Can modify class state
+
+Class Method:
+- Can't modify object instance state
++ Can modify class state
+
+Static Method:
+- Can't modify object instance state
+- Can't modify class state
+'''
+
+class A(object):
+    def foo(self, x):
+        print(f"executing foo({self}, {x})")
+
+    @classmethod
+    def class_foo(cls, x):
+        print(f"executing class_foo({cls}, {x})")
+
+    @staticmethod
+    def static_foo(x):
+        print(f"executing static_foo({x})")
+
+a = A()
+a.foo(1)
+a.class_foo(1)
+A.class_foo(1)
+'''With staticmethods, neither self (the object instance) nor cls (the class) is implicitly passed as the first argument.
+They behave like plain functions except that you can call them from an instance or the class:'''
+a.static_foo(1)
+A.static_foo('hi')
+    
 
 
 
