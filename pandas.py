46ff141b36ce2a3c1889ecb7d6f8f0c50168c774
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2023-06-23 11:54:20 +0530

    Update pandas.py

--- pandas.py
+++ pandas.py
@@ -177,3 +177,327 @@ print(dept_counts_sorted)
 dept_props_sorted = store_depts['department'].value_counts(sort=True, normalize=True)
 print(dept_props_sorted)
 
+# Calc total weekly sales
+sales_all = sales["weekly_sales"].sum()
+
+# Subset for type A stores, calc total weekly sales
+sales_A = sales[sales["type"] == "A"]["weekly_sales"].sum()
+
+# Subset for type B stores, calc total weekly sales
+sales_B = sales[sales["type"] == "B"]["weekly_sales"].sum()
+
+# Subset for type C stores, calc total weekly sales
+sales_C = sales[sales["type"] == "C"]["weekly_sales"].sum()
+
+# Get proportion for each type
+sales_propn_by_type = [sales_A, sales_B, sales_C] / (sales_A+sales_B+sales_C)
+print(sales_propn_by_type)
+> [0.9097747 0.0902253 0. 
+
+ Group by type; calc total weekly sales
+sales_by_type = sales.groupby("type")["weekly_sales"].sum()
+
+# Get proportion for each type
+sales_propn_by_type = sales_by_type/sum(sales.weekly_sales) 
+print(sales_propn_by_type)
+> type
+  A    0.91
+  B    0.09
+  Name: weekly_sales, dtype: float64
+
+# Group by type and is_holiday; calc total weekly sales
+sales_by_type_is_holiday = sales.groupby(["type",'is_holiday'])["weekly_sales"].sum()
+print(sales_by_type_is_holiday)
+> type  is_holiday
+  A     False         2.337e+08
+        True          2.360e+04
+  B     False         2.318e+07
+        True          1.621e+03
+  Name: weekly_sales, dtype: float64
+
+# Import NumPy with the alias np
+import numpy as np
+
+# For each store type, aggregate weekly_sales: get min, max, mean, and median
+sales_stats = sales.groupby('type')['weekly_sales'].agg([np.min, np.max, np.mean, np.median])
+
+# Print sales_stats
+print(sales_stats)
+
+# For each store type, aggregate unemployment and fuel_price_usd_per_l: get min, max, mean, and median
+unemp_fuel_stats = sales.groupby('type')[['unemployment', 'fuel_price_usd_per_l']].agg([np.min, np.max, np.mean, np.median])
+
+# Print unemp_fuel_stats
+print(unemp_fuel_stats)
+
+# Pivot for mean weekly_sales for each store type
+mean_sales_by_type = sales.pivot_table(values='weekly_sales', index='type')
+
+# Print mean_sales_by_type
+print(mean_sales_by_type)
+>       weekly_sales
+  type              
+  A        23674.667
+  B        25696.678
+
+# Pivot for mean and median weekly_sales for each store type
+mean_med_sales_by_type = sales.pivot_table(values='weekly_sales', index= 'type', aggfunc=[np.mean, np.median])
+
+# Print mean_med_sales_by_type
+print(mean_med_sales_by_type)
+>              mean       median
+       weekly_sales weekly_sales
+  type                          
+  A       23674.667     11943.92
+  B       25696.678     13336.08
+
+# Pivot for mean weekly_sales by store type and holiday 
+mean_sales_by_type_holiday = sales.pivot_table(values='weekly_sales', index= 'type', columns='is_holiday')
+
+# Print mean_sales_by_type_holiday
+print(mean_sales_by_type_holiday)
+> is_holiday      False    True 
+  type                          
+  A           23768.584  590.045
+  B           25751.981  810.705
+
+# Print mean weekly_sales by department and type; fill missing values with 0
+print(sales.pivot_table(values='weekly_sales', index='department', columns='type', fill_value=0))
+
+# Print the mean weekly_sales by department and type; fill missing values with 0s; sum all rows and cols
+print(sales.pivot_table(values="weekly_sales", index="department", columns="type", fill_value=0, margins=True))
+
+# Look at temperatures
+print(temperatures)
+
+# Index temperatures by city
+temperatures_ind = temperatures.set_index('city')
+
+# Look at temperatures_ind
+print(temperatures_ind)
+
+# Reset the index, keeping its contents
+print(temperatures_ind.reset_index())
+
+# Reset the index, dropping its contents
+print(temperatures_ind.reset_index(drop=True))
+
+# Make a list of cities to subset on
+cities = ['Moscow', 'Saint Petersburg']
+
+# Subset temperatures using square brackets
+print(temperatures[temperatures['city'].isin(cities)])
+
+# Subset temperatures_ind using .loc[]
+print(temperatures_ind.loc[cities])
+
+# Index temperatures by country & city
+temperatures_ind = temperatures.set_index(["country","city"])
+
+# List of tuples: Brazil, Rio De Janeiro & Pakistan, Lahore
+rows_to_keep = [("Brazil", "Rio De Janeiro"),("Pakistan","Lahore")]
+
+# Subset for rows to keep
+print(temperatures_ind.loc[rows_to_keep])
+
+# Sort temperatures_ind by index values
+print(temperatures_ind.sort_index())
+
+# Sort temperatures_ind by index values at the city level
+print(temperatures_ind.sort_index(level='city'))
+
+# Sort temperatures_ind by country then descending city
+print(temperatures_ind.sort_index(level=['country','city'], ascending=[True, False]))
+
+# Sort the index of temperatures_ind
+temperatures_srt = temperatures_ind.sort_index()
+
+# Subset rows from Pakistan to Russia
+print(temperatures_srt.loc['Pakistan':'Russia'])
+
+# Try to subset rows from Lahore to Moscow (This will return nonsense.)
+print(temperatures_srt.loc['Lahore':'Moscow'])
+
+# Subset rows from Pakistan, Lahore to Russia, Moscow
+print(temperatures_srt.loc[('Pakistan','Lahore'):('Russia', 'Moscow')])
+
+# Subset rows from India, Hyderabad to Iraq, Baghdad
+print(temperatures_srt.loc[('India', 'Hyderabad'):('Iraq', 'Baghdad')])
+
+# Subset columns from date to avg_temp_c
+print(temperatures_srt.loc[:, 'date':'avg_temp_c'])
+
+# Subset in both directions at once
+# Subset columns from date to avg_temp_c
+print(temperatures_srt.loc[('India', 'Hyderabad'):('Iraq', 'Baghdad'), 'date':'avg_temp_c'])
+
+# Use Boolean conditions to subset temperatures for rows in 2010 and 2011
+temperatures_bool = temperatures[(temperatures["date"] >= '2010-01-01') & (temperatures["date"] <= '2011-12-31')]
+print(temperatures_bool)
+
+# Set date as an index
+temperatures_ind = temperatures.set_index('date')
+
+# Use .loc[] to subset temperatures_ind for rows in 2010 and 2011
+print(temperatures_ind.loc['2010':'2011'])
+
+# Use .loc[] to subset temperatures_ind for rows from Aug 2010 to Feb 2011
+print(temperatures_ind.loc['2010-08':'2011-2'])
+
+# Get 23rd row, 2nd column (index 22, 1)
+print(temperatures.iloc[22,2])
+
+# Use slicing to get the first 5 rows
+print(temperatures.iloc[0:5,:])
+
+# Use slicing to get columns 3 to 4
+print(temperatures.iloc[:,2:4])
+
+# Use slicing in both directions at once
+print(temperatures.iloc[0:5,2:4])
+
+# Add a year column to temperatures
+temperatures['year'] = temperatures['date'].dt.year
+
+# Pivot avg_temp_c by country and city vs year
+temp_by_country_city_vs_year = temperatures.pivot_table('avg_temp_c', index=['country','city'], columns='year')
+
+# See the result
+print(temp_by_country_city_vs_year)
+
+# Subset for Egypt to India
+temp_by_country_city_vs_year.loc['Egypt':'India']
+
+# Subset for Egypt, Cairo to India, Delhi
+temp_by_country_city_vs_year.loc[('Egypt','Cairo'):('India','Delhi')]
+
+# Subset in both directions at once
+temp_by_country_city_vs_year.loc[('Egypt','Cairo'):('India','Delhi'),'2005':'2010']
+
+# Get the worldwide mean temp by year
+mean_temp_by_year = temp_by_country_city_vs_year.mean()
+
+# Filter for the year that had the highest mean temp
+print(mean_temp_by_year[mean_temp_by_year==mean_temp_by_year.max()])
+> year
+  2013    20.312
+  dtype: float64
+
+# Get the mean temp by city
+mean_temp_by_city = temp_by_country_city_vs_year.mean(axis="columns")
+
+# Filter for the city that had the lowest mean temp
+print(mean_temp_by_city[mean_temp_by_city==mean_temp_by_city.min()])
+> country  city  
+  China    Harbin    4.877
+  dtype: float64
+
+# Import matplotlib.pyplot with alias plt
+import matplotlib.pyplot as plt
+
+# Print a DataFrame that shows whether each value in avocados_2016 is missing or not.
+print(avocados_2016.isna())
+
+# Print a summary that shows whether any value in each column is missing or not.
+print(avocados_2016.isna().any())
+
+# Bar plot of missing values by variable
+avocados_2016.isna().sum().plot(kind='bar')
+
+# Show plot
+plt.show()
+
+# Remove rows with missing values
+avocados_complete = avocados_2016.dropna()
+
+# Check if any columns contain missing values
+print(avocados_complete.isna().any())
+> date               False
+  avg_price          False
+  total_sold         False
+  small_sold         False
+  large_sold         False
+  xl_sold            False
+  total_bags_sold    False
+  small_bags_sold    False
+  large_bags_sold    False
+  xl_bags_sold       False
+  dtype: bool
+
+# From previous step
+cols_with_missing = ["small_sold", "large_sold", "xl_sold"]
+avocados_2016[cols_with_missing].hist()
+plt.show()
+
+# Fill in missing values with 0
+avocados_filled = avocados_2016.fillna(0)
+
+# Create histograms of the filled columns
+avocados_filled[cols_with_missing].hist()
+
+# Show the plot
+plt.show()
+
+# Create a list of dictionaries with new data
+avocados_list = [
+    {'date': "2019-11-03", 'small_sold': 10376832, 'large_sold': 7835071},
+    {'date': "2019-11-10", 'small_sold': 10717154, 'large_sold': 8561348},
+]
+
+# Convert list into DataFrame
+avocados_2019 = pd.DataFrame(avocados_list)
+
+# Print the new DataFrame
+print(avocados_2019)
+>          date  small_sold  large_sold
+  0  2019-11-03    10376832     7835071
+  1  2019-11-10    10717154     8561348
+
+# Create a dictionary of lists with new data
+avocados_dict = {
+  "date": ["2019-11-17", "2019-12-01"],
+  "small_sold": [10859987, 9291631],
+  "large_sold": [7674135, 6238096]
+}
+
+# Convert dictionary into DataFrame
+avocados_2019 = pd.DataFrame(avocados_dict)
+
+# Print the new DataFrame
+print(avocados_2019)
+>          date  small_sold  large_sold
+  0  2019-11-17    10859987     7674135
+  1  2019-12-01     9291631     6238096
+
+# Read CSV as DataFrame called airline_bumping
+airline_bumping = pd.read_csv("airline_bumping.csv")
+
+# Take a look at the DataFrame
+print(airline_bumping.head())
+>              airline  year  nb_bumped  total_passengers
+  0    DELTA AIR LINES  2017        679          99796155
+  1     VIRGIN AMERICA  2017        165           6090029
+  2    JETBLUE AIRWAYS  2017       1475          27255038
+  3    UNITED AIRLINES  2017       2067          70030765
+  4  HAWAIIAN AIRLINES  2017         92           8422734
+
+# For each airline, select nb_bumped and total_passengers and sum
+airline_totals = airline_bumping.groupby("airline")[["nb_bumped", "total_passengers"]].sum()
+
+# Create new col, bumps_per_10k: no. of bumps per 10k passengers for each airline
+airline_totals["bumps_per_10k"] = airline_totals["nb_bumped"] / airline_totals["total_passengers"] * 10000
+
+# Print airline_totals
+print(airline_totals)
+
+# Create airline_totals_sorted
+airline_totals_sorted = airline_totals.sort_values('bumps_per_10k', ascending=False)
+
+# Print airline_totals_sorted
+print(airline_totals_sorted)
+
+# Save as airline_totals_sorted.csv
+airline_totals_sorted.to_csv("airline_totals_sorted.csv")
+
+
+# https://yuleii.github.io/2020/06/27/data-manipulation-with-pandas.html
