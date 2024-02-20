9b9529e828bfee45a68b6e24c34fde0d4e13675a
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2024-02-20 17:54:10 +0530

    Update qualcomm.py

--- qualcomm.py
+++ qualcomm.py
@@ -1,4 +1,5 @@
 Q1 Write a Program to combine two different dictionaries. While combining, if you find the same keys, you can add the values of these same keys. Output the new dictionary
+
 Ans:
 d1 = {'key1': 50, 'key2': 100, 'key3':200}
 d2 = {'key1': 200, 'key2': 100, 'key4':300}
