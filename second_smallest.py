6c8491c1865924e0ba635d0eda0c59b976a11919
Sudhanshu Chaudhary <73557822+sudh29@users.noreply.github.com>
2024-04-27 12:53:04 +0530

    Delete second_smallest.py

--- second_smallest.py
+++
@@ -1,8 +0,0 @@
-def second_smallest(numbers):
-    m1 = m2 = float('inf')
-    for x in numbers:
-        if x <= m1:
-            m1, m2 = x, m1
-        elif x < m2:
-            m2 = x
-    return m2
